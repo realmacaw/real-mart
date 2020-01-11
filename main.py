5401b0f1883e00c42c24a0e06c94570983af1b85
realmacaw <59328122+realmacaw@users.noreply.github.com>
2020-01-11 15:39:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+__author__ = "macaw"
+import os
+from tkinter import *
+from tkinter import messagebox
+
+main = Tk()
+main.geometry("1366x768")
+main.title("Big Bazaar")
+main.resizable(0, 0)
+def Exit():
+    sure = messagebox.askyesno("Exit","Are you sure you want to exit?", parent=main)
+    if sure == True:
+        main.destroy()
+        
+main.protocol("WM_DELETE_WINDOW", Exit)
+
+def emp():
+    main.withdraw()
+    os.system("python employee.py")
+    main.deiconify()
+
+
+def adm():
+    main.withdraw()
+    os.system("python admin.py")
+    main.deiconify()
+
+label1 = Label(main)
+label1.place(relx=0, rely=0, width=1366, height=768)
+img = PhotoImage(file="main.png")
+label1.configure(image=img)
+
+button1 = Button(main)
+button1.place(relx=0.316, rely=0.446, width=146, height=90)
+button1.configure(relief="flat")
+button1.configure(overrelief="flat")
+button1.configure(activebackground="#ffffff")
+button1.configure(cursor="hand2")
+button1.configure(foreground="#ffffff")
+button1.configure(background="#ffffff")
+button1.configure(borderwidth="0")
+img2 = PhotoImage(file="1.png")
+button1.configure(image=img2)
+button1.configure(command=emp)
+
+button2 = Button(main)
+button2.place(relx=0.566, rely=0.448, width=146, height=90)
+button2.configure(relief="flat")
+button2.configure(overrelief="flat")
+button2.configure(activebackground="#ffffff")
+button2.configure(cursor="hand2")
+button2.configure(foreground="#ffffff")
+button2.configure(background="#ffffff")
+button2.configure(borderwidth="0")
+img3 = PhotoImage(file="2.png")
+button2.configure(image=img3)
+button2.configure(command=adm)
+
+main.mainloop()
